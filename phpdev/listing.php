da2b40b19c78b9415d3e7bd6c2c8fffe0e4c63d7
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2008-08-26 16:21:09 +0000

    adding the detail listing page for the individual properties.

---
+++ phpdev/listing.php
@@ -0,0 +1,13 @@
+<?php
+// --- SET THIS VARIABLE FOR THE PAGE TITLE --- //
+$page_title = 'Hideaway Vacation Homes - Property Listing';
+
+// --- HEADER = TOP NAV + LOGO - OPENS ALL PAGES --- //
+// --- COLUMN_SPACER IS THE 15 PIXEL SPACER IN BETWEEN COLUMNS --- //
+// --- FOOTER IS THE PHILANTHROPY AND MOUNTAIN IMAGE - CLOSES ALL PAGES --- //
+// --- ANYTHING CALLED INBETWEEN HEADER AND FOOTER IS THE MAIN BODY OF THE PAGE --- //
+
+include "template/header.inc";
+include "content/listing.inc";
+include "template/footer.inc";
+?>

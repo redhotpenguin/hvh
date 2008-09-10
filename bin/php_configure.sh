946004a97e8430cfb6a5903ae42a4eada7a74d14
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2008-09-10 19:31:03 +0000

    finally it builds with ssl

--- bin/php_configure.sh
+++ bin/php_configure.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./configure --enable-soap --with-apxs2=/usr/sbin/apxs  --prefix=$HOME/dev/php --with-mysql-sock=/opt/local/var/run/mysql5/mysqld.sock --with-openssl
+./configure --enable-soap --with-apxs2=/usr/sbin/apxs  --prefix=$HOME/dev/php --with-mysql-sock=/opt/local/var/run/mysql5/mysqld.sock --with-openssl-dir=/opt/local/etc/openssl

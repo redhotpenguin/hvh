345579f59f646b83cb76861aa950388755d54adc
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2008-09-10 21:30:32 +0000

    monkey tool go eep! php integration working!

--- bin/sforce_test.php
+++ bin/sforce_test.php
@@ -2,17 +2,31 @@
 require_once ('soapclient/SforcePartnerClient.php');
 
 $mySforceConnection = new SforcePartnerClient();
+
+echo "opening connection\n";
 $mySoapClient = $mySforceConnection->createConnection("partner.wsdl.xml");
+
+echo "logging in\n";
 $mylogin = $mySforceConnection->login("fred@redhotpenguin.com", "yomaingJN9fVMtleBighIslxY3EZxuE");
+
+echo "logged in ok, running query\n";
 
 $query = "SELECT Id, FirstName, LastName from Contact";
 $queryResult = $mySforceConnection->query($query);
+
+echo "query finished\n";
 $records = $queryResult->records;
+
+echo "records retrieved\n";
 foreach ($records as $record) {
       $sObject = new SObject($record);
-        echo "Id = ".$sObject->Id;
-        echo "First Name = ".$sObject->fields->FirstName;
-          echo "Last Name = ".$sObject->fields->LastName;
+      echo "\n";
+      echo "Id = ".$sObject->Id;
+      echo "\n";
+      echo "First Name = ".$sObject->fields->FirstName;
+      echo "\n";
+      echo "Last Name = ".$sObject->fields->LastName;
+      echo "\n";
 }
 
 

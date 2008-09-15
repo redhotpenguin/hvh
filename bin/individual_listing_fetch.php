35b42bc313255e4ff921e4f205b2820e89e5a2bb
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2008-09-15 00:02:50 +0000

    grab amenities fields

--- bin/individual_listing_fetch.php
+++ bin/individual_listing_fetch.php
@@ -25,7 +25,24 @@ foreach ($prop_array as $prop) {
 
 	// PUTTING EACH PROPERTY OBJ IN STATE WITH KEY EQUAL TO SFORCE ID //
 
-	$single_query = "SELECT Id, Name, Property_Address__c, Category__c, Description__c, Location__c, Image_URL_1__c, Image_URL_2__c, Image_URL_3__c, Image_URL_4__c, Image_URL_5__c, Image_URL_6__c, Image_URL_7__c, Image_URL_8__c, City__c from Property__c where Id='$prop_obj->Id' limit 1";
+    $amenities_array = array("Wireless_Internet__c", "Private_Hot_Tub__c", 
+	"Swimming_Pool__c", "High_Definition_TV_DVR__c",
+	"Unlimited_Long_Distance__c",
+	"Stereo_System__c", "CD_Player__c", "Music_Library__c", "VCR__c",
+	"DVD_Player__c",
+	"Video_Library__c", "Bath_Towels__c", "Bed_Linens__c", "Washer_Dryer__c",
+	"Refrigerator__c", "Dishwasher__c", "Oven__c",
+	"Microwave__c", "Air_Conditioning__c",
+	"Full_Kitchen__c", "Cooking_Utensils__c", "Barbecue__c", "Hair_Dryer__c" );
+
+
+	$q_fields = "";
+	foreach ($amenities_array as $amenity) {
+	    $q_fields = $q_fields.", ".$amenity;
+        }
+#	error_log("\nQ Fields is $q_fields");
+
+	$single_query = "SELECT Id, Name, Property_Address__c, Category__c, Description__c, Location__c, Image_URL_1__c, Image_URL_2__c, Image_URL_3__c, Image_URL_4__c, Image_URL_5__c, Image_URL_6__c, Image_URL_7__c, Image_URL_8__c, City__c $q_fields from Property__c where Id='$prop_obj->Id' limit 1";
 	$single_results = $mySforceConnection->query($single_query);
 	$single = $single_results->records;
 

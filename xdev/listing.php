869dda01f1d129e897cac95313d8abc705aaa1ac
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2009-01-29 19:43:18 +0000

    add the new development into subversion

---
+++ xdev/listing.php
@@ -0,0 +1,13 @@
+<?php
+// --- SET THIS VARIABLE FOR THE PAGE TITLE --- //
+$page_title = 'Hideaway Vacation Homes - Property Listing';
+$left_cols = 'listing_detail';
+$sub_nav = 'listing_sub';
+$content = '../phpdev/xdev_content_listing';
+// --- HEADER = TOP NAV + LOGO - OPENS ALL PAGES --- //
+// --- COLUMN_SPACER IS THE 15 PIXEL SPACER IN BETWEEN COLUMNS --- //
+// --- FOOTER IS THE PHILANTHROPY AND MOUNTAIN IMAGE - CLOSES ALL PAGES --- //
+// --- ANYTHING CALLED INBETWEEN HEADER AND FOOTER IS THE MAIN BODY OF THE PAGE --- //
+include "template/page.inc";
+
+?>
---
+++ xdev/love_life.php
@@ -0,0 +1,17 @@
+<?php
+// --- SET THIS VARIABLE FOR THE PAGE TITLE --- //
+$page_title = 'Hideaway Vacation Homes';
+$left_cols = 'home';
+$sub_nav = 'love_life_sub';
+$content = 'content/love_life';
+
+// switch to this when appropriate.
+// $content = 'solutions.inc';
+
+// --- HEADER = TOP NAV + LOGO - OPENS ALL PAGES --- //
+// --- COLUMN_SPACER IS THE 15 PIXEL SPACER IN BETWEEN COLUMNS --- //
+// --- FOOTER IS THE PHILANTHROPY AND MOUNTAIN IMAGE - CLOSES ALL PAGES --- //
+// --- ANYTHING CALLED INBETWEEN HEADER AND FOOTER IS THE MAIN BODY OF THE PAGE --- //
+
+include "template/page.inc";
+?>

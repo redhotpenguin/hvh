45ab4a60e3b8e971475d5eafedde7936f6abd065
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2009-05-13 17:19:57 +0000

    adding partner pages

---
+++ xdev/solutions_force.php
@@ -0,0 +1,17 @@
+<?php
+// --- SET THIS VARIABLE FOR THE PAGE TITLE --- //
+$page_title = 'Hideaway Vacation Homes';
+$left_cols = 'home';
+$sub_nav = 'solutions_property_manager_sub';
+$content = 'content/solutions_force';
+$banner = 'main_image.inc';
+// switch to this when appropriate.
+// $content = 'solutions.inc';
+
+// --- HEADER = TOP NAV + LOGO - OPENS ALL PAGES --- //
+// --- COLUMN_SPACER IS THE 15 PIXEL SPACER IN BETWEEN COLUMNS --- //
+// --- FOOTER IS THE PHILANTHROPY AND MOUNTAIN IMAGE - CLOSES ALL PAGES --- //
+// --- ANYTHING CALLED INBETWEEN HEADER AND FOOTER IS THE MAIN BODY OF THE PAGE --- //
+
+include "template/page.inc";
+?>

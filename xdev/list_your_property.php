2ca44b876fc4328a865aa94eff49b5586ea17745
(no author) <(no author)@f237f3bc-e152-0410-a08d-852af2c4d981>
2011-01-16 00:57:21 +0000

    update

--- xdev/list_your_property.php
+++ xdev/list_your_property.php
@@ -1,15 +1,17 @@
 <?php
 // --- SET THIS VARIABLE FOR THE PAGE TITLE --- //
-$page_title = 'Hideaway Vacation Homes - Property Listing';
-$left_cols = 'listing_detail';
-$sub_nav = 'listing_sub';
-$content = '../phpdev/xdev_content_listing2';
+$page_title = 'Hideaway Vacation Homes';
+$left_cols = 'home';
+$sub_nav = 'solutions_hotels_inns_sub';
+$content = 'content/upload/list_step_one';
 $banner = 'main_image.inc';
-$no_left = 'no_left';
+// switch to this when appropriate.
+// $content = 'solutions.inc';
+
 // --- HEADER = TOP NAV + LOGO - OPENS ALL PAGES --- //
 // --- COLUMN_SPACER IS THE 15 PIXEL SPACER IN BETWEEN COLUMNS --- //
 // --- FOOTER IS THE PHILANTHROPY AND MOUNTAIN IMAGE - CLOSES ALL PAGES --- //
 // --- ANYTHING CALLED INBETWEEN HEADER AND FOOTER IS THE MAIN BODY OF THE PAGE --- //
-include "template/page.inc";
 
+include "template/page.inc";
 ?>
